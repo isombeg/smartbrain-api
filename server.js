4a8b290af3c4649859d214b93cc997d70691b5ac
root <root@gj-isombe.localdomain>
2020-05-27 18:29:29 -0400

    integrating server with database

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
 const express = require('express');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
+const knex = require('knex');
+
+const db = knex({
+    client: 'pg',
+    connection: {
+      host : '127.0.0.1',
+      user : 'isombeg',
+      password : 'nyamanayo',
+      database : 'smartbrain'
+    }
+  }
+);
 
 const app = express();
 app.use(express.json());
@@ -65,18 +77,23 @@ app.post(
     '/register',
     (req, res) => {
         const {email, name, password} = req.body;
-        database.users.push(
-            {
-                id: '125',
-                name: name,
-                email: email,
-                password: password,
-                entries: 0,
-                joined: new Date()
-            }
-        )
-        res.json(database.users[database.users.length - 1]);
-        console.log(database.users);
+        db('users')
+            .returning('*')
+            .insert(
+                {
+                    email: email,
+                    name: name,
+                    joined: new Date()
+                }
+            )
+            .then(
+                user => {
+                    res.json(user[0]);
+                }
+            )
+            .catch(
+                err => res.status(400).json('Unable to register')
+            )
     }
 )
 
